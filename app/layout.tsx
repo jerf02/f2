8b034fa0ea5ae025fef8f1004ae898c8fccbd33a
jerf02 <jerf02@hotmail.com>
2025-04-16 13:36:24 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
+// app/layout.tsx
+import './globals.css'
+import { ReactNode } from 'react'
+import type { Metadata } from 'next'
 
-import { ReactNode } from "react";
-
-export const metadata = {
+export const metadata: Metadata = {
   title: 'FOKO LLC',
   description: 'Site oficial da FOKO LLC - Pisos, azulejos e remodelações.',
-};
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt">
       <body>{children}</body>
     </html>
-  );
+  )
 }
